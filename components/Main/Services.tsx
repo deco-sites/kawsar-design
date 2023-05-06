279eb897046fe5f7c12ed5b74eaad6506f813fd1
Sid Nascimento <sid.nas@outlook.com>
2023-05-06 18:30:02 -0300

    Melhorias Cards

--- components/Main/Services.tsx
+++ components/Main/Services.tsx
@@ -18,30 +18,30 @@ export default function Services({ serviceText, card }: Props) {
   return (
     <section>
       <div class="w-full bg-primary ">
-        <div class="py-28 relative">
-          <h3 class="pb-8 text-2xl text-center bg-gradient-to-r from-white to-gray-700 text-transparent bg-clip-text">
+        <div class="py-32 relative">
+          <h3 class="pb-10 text-2xl text-center bg-gradient-to-r from-white to-gray-700 text-transparent bg-clip-text">
             {serviceText}
           </h3>
 
           <div class="h-full">
             <div
               id={id}
-              class="container max-w-[1024px] mx-auto overflow-hidden lg:px-10"
+              class="container max-w-[1024px] mx-auto overflow-hidden"
             >
               <div
-                class="grid grid-cols-2 gap-3 lg:(flex justify-center) overflow-hidden"
+                class="grid grid-cols-2 gap-2 lg:(flex justify-center) md:gap-5 overflow-hidden"
                 data-slider-content
               >
                 {card.map((card, idx) => {
                   return (
                     <div
                       id={`${id}-${idx}`}
-                      class="p-4 grid grid-cols-1 h-80 lg:w-56 border rounded-sm"
+                      class="grid grid-cols-1 h-80 lg:(h-[340px] w-[260px]) border border-gray-800 rounded-sm pl-4 pb-8"
                     >
-                      <div class="flex flex-col justify-end items-start text-secundary">
-                        <img src={card.cardImg} class="h-10" />
-                        <h4 class="font-semibold mt-4">{card.cardTitle}</h4>
-                        <p class="text-sm overflow-hidden overflow-ellipsis mb-4 mt-4">
+                      <div class="flex flex-col justify-end items-start text-secundary mr-2">
+                        <img src={card.cardImg} class="h-8" />
+                        <h4 class="bg-gradient-to-r from-white to-gray-500 text-transparent bg-clip-text font-semibold mt-5">{card.cardTitle}</h4>
+                        <p class="text-sm overflow-hidden overflow-ellipsis mb-4 mt-5 whitespace-pre-line">
                           {card.cardText}
                         </p>
                       </div>

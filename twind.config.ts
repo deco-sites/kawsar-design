35719506ac3da893a1940ce19321997159cefffe
Sid Nascimento <sid.nas@outlook.com>
2023-05-08 13:29:56 -0300

    Background Color

--- twind.config.ts
+++ twind.config.ts
@@ -10,7 +10,7 @@ export default {
         "2xl": "1536px",
       },
       colors: {
-        primary: "#111827",
+        primary: "#000000",
         "primary-dark": "#09090b",
         "primary-light": "#a1a1aa",
         secundary: "#71717a",
